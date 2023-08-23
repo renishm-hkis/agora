badc6cea0bf26b640f20a3610d7a028f9fb7d739
renishm-hkis <138757819+renishm-hkis@users.noreply.github.com>
2023-08-23 16:54:44 +0530

    init

---
+++ src/examples/basic/JoinChannelVideo/JoinChannelVideo.tsx
@@ -0,0 +1,278 @@
+import React, { ReactElement } from 'react';
+import { Platform } from 'react-native';
+import {
+  ChannelProfileType,
+  ClientRoleType,
+  ErrorCodeType,
+  IRtcEngineEventHandler,
+  LocalVideoStreamError,
+  LocalVideoStreamState,
+  RtcConnection,
+  RtcStats,
+  RtcSurfaceView,
+  RtcTextureView,
+  UserOfflineReasonType,
+  VideoCanvas,
+  VideoSourceType,
+  VideoViewSetupMode,
+  createAgoraRtcEngine,
+} from 'react-native-agora';
+
+import {
+  BaseComponent,
+  BaseVideoComponentState,
+} from '../../../components/BaseComponent';
+import {
+  AgoraButton,
+  AgoraDivider,
+  AgoraDropdown,
+  AgoraStyle,
+  AgoraSwitch,
+} from '../../../components/ui';
+import Config from '../../../config/agora.config';
+import { enumToItems } from '../../../utils';
+import { askMediaAccess } from '../../../utils/permissions';
+
+interface State extends BaseVideoComponentState {
+  switchCamera: boolean;
+  renderByTextureView: boolean;
+  setupMode: VideoViewSetupMode;
+}
+
+export default class JoinChannelVideo
+  extends BaseComponent<{}, State>
+  implements IRtcEngineEventHandler
+{
+  protected createState(): State {
+    return {
+      appId: Config.appId,
+      enableVideo: true,
+      channelId: Config.channelId,
+      token: Config.token,
+      uid: Config.uid,
+      joinChannelSuccess: false,
+      remoteUsers: [],
+      startPreview: false,
+      switchCamera: false,
+      renderByTextureView: false,
+      setupMode: VideoViewSetupMode.VideoViewSetupReplace,
+    };
+  }
+
+  /**
+   * Step 1: initRtcEngine
+   */
+  protected async initRtcEngine() {
+    const { appId } = this.state;
+    if (!appId) {
+      this.error(`appId is invalid`);
+    }
+
+    this.engine = createAgoraRtcEngine();
+    this.engine.initialize({
+      appId,
+      logConfig: { filePath: Config.logFilePath },
+      // Should use ChannelProfileLiveBroadcasting on most of cases
+      channelProfile: ChannelProfileType.ChannelProfileLiveBroadcasting,
+    });
+    this.engine.registerEventHandler(this);
+
+    // Need granted the microphone and camera permission
+    await askMediaAccess([
+      'android.permission.RECORD_AUDIO',
+      'android.permission.CAMERA',
+    ]);
+
+    // Need to enable video on this case
+    // If you only call `enableAudio`, only relay the audio stream to the target channel
+    this.engine.enableVideo();
+
+    // Start preview before joinChannel
+    this.engine.startPreview();
+    this.setState({ startPreview: true });
+  }
+
+  /**
+   * Step 2: joinChannel
+   */
+  protected joinChannel() {
+    const { channelId, token, uid } = this.state;
+    if (!channelId) {
+      this.error('channelId is invalid');
+      return;
+    }
+    if (uid < 0) {
+      this.error('uid is invalid');
+      return;
+    }
+
+    // start joining channel
+    // 1. Users can only see each other after they join the
+    // same channel successfully using the same app id.
+    // 2. If app certificate is turned on at dashboard, token is needed
+    // when joining channel. The channel name and uid used to calculate
+    // the token has to match the ones used for channel join
+    this.engine?.joinChannel(token, channelId, uid, {
+      // Make myself as the broadcaster to send stream to remote
+      clientRoleType: ClientRoleType.ClientRoleBroadcaster,
+    });
+  }
+
+  /**
+   * Step 3 (Optional): switchCamera
+   */
+  switchCamera = () => {
+    const { switchCamera } = this.state;
+    this.engine?.switchCamera();
+    this.setState({ switchCamera: !switchCamera });
+  };
+
+  /**
+   * Step 4: leaveChannel
+   */
+  protected leaveChannel() {
+    this.engine?.leaveChannel();
+  }
+
+  /**
+   * Step 5: releaseRtcEngine
+   */
+  protected releaseRtcEngine() {
+    this.engine?.unregisterEventHandler(this);
+    this.engine?.release();
+  }
+
+  onError(err: ErrorCodeType, msg: string) {
+    super.onError(err, msg);
+  }
+
+  onJoinChannelSuccess(connection: RtcConnection, elapsed: number) {
+    super.onJoinChannelSuccess(connection, elapsed);
+  }
+
+  onLeaveChannel(connection: RtcConnection, stats: RtcStats) {
+    super.onLeaveChannel(connection, stats);
+  }
+
+  onUserJoined(connection: RtcConnection, remoteUid: number, elapsed: number) {
+    super.onUserJoined(connection, remoteUid, elapsed);
+  }
+
+  onUserOffline(
+    connection: RtcConnection,
+    remoteUid: number,
+    reason: UserOfflineReasonType
+  ) {
+    super.onUserOffline(connection, remoteUid, reason);
+  }
+
+  onVideoDeviceStateChanged(
+    deviceId: string,
+    deviceType: number,
+    deviceState: number
+  ) {
+    this.info(
+      'onVideoDeviceStateChanged',
+      'deviceId',
+      deviceId,
+      'deviceType',
+      deviceType,
+      'deviceState',
+      deviceState
+    );
+  }
+
+  onLocalVideoStateChanged(
+    source: VideoSourceType,
+    state: LocalVideoStreamState,
+    error: LocalVideoStreamError
+  ) {
+    this.info(
+      'onLocalVideoStateChanged',
+      'source',
+      source,
+      'state',
+      state,
+      'error',
+      error
+    );
+  }
+
+  protected renderUsers(): ReactElement | undefined {
+    return super.renderUsers();
+  }
+
+  protected renderVideo(user: VideoCanvas): ReactElement | undefined {
+    const { renderByTextureView, setupMode } = this.state;
+    return renderByTextureView ? (
+      <RtcTextureView
+        style={user.uid === 0 ? AgoraStyle.videoLarge : AgoraStyle.videoSmall}
+        canvas={{ ...user, setupMode }}
+      />
+    ) : (
+      <RtcSurfaceView
+        style={user.uid === 0 ? AgoraStyle.videoLarge : AgoraStyle.videoSmall}
+        zOrderMediaOverlay={user.uid !== 0}
+        canvas={{ ...user, setupMode }}
+      />
+    );
+  }
+
+  protected renderConfiguration(): ReactElement | undefined {
+    const { startPreview, joinChannelSuccess, renderByTextureView, setupMode } =
+      this.state;
+    return (
+      <>
+        <AgoraSwitch
+          disabled={
+            (!startPreview && !joinChannelSuccess) || Platform.OS !== 'android'
+          }
+          title={`renderByTextureView`}
+          value={renderByTextureView}
+          onValueChange={(value) => {
+            this.setState({ renderByTextureView: value });
+          }}
+        />
+        <AgoraDivider />
+        <AgoraDropdown
+          title={'setupMode'}
+          items={enumToItems(VideoViewSetupMode)}
+          value={setupMode}
+          onValueChange={(value) => {
+            this.setState({ setupMode: value });
+          }}
+        />
+        {setupMode === VideoViewSetupMode.VideoViewSetupAdd ? (
+          <>
+            <AgoraDivider />
+            {renderByTextureView ? (
+              <RtcTextureView
+                style={AgoraStyle.videoSmall}
+                canvas={{ uid: 0, setupMode }}
+              />
+            ) : (
+              <RtcSurfaceView
+                style={AgoraStyle.videoSmall}
+                canvas={{ uid: 0, setupMode }}
+              />
+            )}
+          </>
+        ) : undefined}
+        <AgoraDivider />
+      </>
+    );
+  }
+
+  protected renderAction(): ReactElement | undefined {
+    const { startPreview, joinChannelSuccess } = this.state;
+    return (
+      <>
+        <AgoraButton
+          disabled={!startPreview && !joinChannelSuccess}
+          title={`switchCamera`}
+          onPress={this.switchCamera}
+        />
+      </>
+    );
+  }
+}
